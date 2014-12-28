36fc7874be156ac812091d463852cbe77cae8d1e
maslu02 <maslu02@ca.com>
2014-12-28 01:39:28 +0100

    Javadoc for Golem common data structure updated

--- src/cz/a_d/automation/golem/common/AddressArrayList.java
+++ src/cz/a_d/automation/golem/common/AddressArrayList.java
@@ -1,6 +1,4 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
  */
 package cz.a_d.automation.golem.common;
 
@@ -9,32 +7,39 @@ import java.util.ArrayList;
 import java.util.Collection;
 
 /**
+ * Extension of Arraylist which is comparing stored objects by using address of object not by using method equal. This allow to store
+ * multiple instances of same object and safely get index of instance which you currently have. This feature is used by Golem iterators for
+ * keeping and safely manipulating actions in stream.
  *
- * @param <T>
  * @author casper
+ * @param <T> the type of elements in this list.
  */
 public class AddressArrayList<T> extends ArrayList<T> {
 
     private static final long serialVersionUID = 1L;
 
     /**
+     * Constructs an empty list with the specified initial capacity.
      *
-     * @param initialCapacity
+     * @param initialCapacity the initial capacity of the list
+     * @throws IllegalArgumentException if the specified initial capacity is negative
      */
     public AddressArrayList(int initialCapacity) {
         super(initialCapacity);
     }
 
     /**
-     *
+     * Constructs an empty list with an initial capacity of ten.
      */
     public AddressArrayList() {
         super();
     }
 
     /**
+     * Constructs a list containing the elements of the specified collection, in the order they are returned by the collection's iterator.
      *
-     * @param c
+     * @param c the collection whose elements are to be placed into this list
+     * @throws NullPointerException if the specified collection is null
      */
     public AddressArrayList(Collection<? extends T> c) {
         super(c);
--- src/cz/a_d/automation/golem/common/FastStack.java
+++ src/cz/a_d/automation/golem/common/FastStack.java
@@ -6,51 +6,60 @@ import java.util.Collection;
 import java.util.LinkedList;
 
 /**
- * This class is replacement for Stack which slow because has protection for 
- * save multi-thread access to data. It is implemented like extension for 
- * LinkedList. This extension has just more obvious name and implement methods 
- * specific for stack by reusing LinkedList methods.
- * 
+ * This class is replacement for Stack which slow because has protection for save multi-thread access to data. It is implemented like
+ * extension for LinkedList. This extension has just more obvious name and implement methods specific for stack by reusing LinkedList
+ * methods.
+ *
  * @author casper
+ * @param <E> the type of elements in this stack.
  */
 public class FastStack<E> extends LinkedList<E> {
     private static final long serialVersionUID = 1L;
 
+    /**
+     * Constructs an empty stack.
+     */
     public FastStack() {
         super();
     }
 
+    /**
+     * Constructs a stack containing the elements of the specified collection, in the order they are returned by the collection's iterator.
+     *
+     * @param c the collection whose elements are to be placed into this list
+     * @throws NullPointerException if the specified collection is null
+     */
     public FastStack(Collection<? extends E> c) {
         super(c);
     }
- 
+
     /**
-     * This method allows access to object on top of stack, object stay in stack.
-     * 
+     * Method allows access to object on top of stack, object stay in stack.
+     *
      * @return Object on the top of stack
      */
     @Override
-    public E peek(){
+    public E peek() {
         return super.peek();
     }
-    
+
     /**
      * Method inserting object into stack on first position.
-     * @param item 
+     *
+     * @param item
      */
-    
     @Override
-    public void push(E item){
+    public void push(E item) {
         super.push(item);
     }
-    
+
     /**
      * Method return and remove first object in stack.
-     * 
+     *
      * @return first object in stack before method call.
      */
     @Override
-    public E pop(){
+    public E pop() {
         return super.pop();
     }
 
