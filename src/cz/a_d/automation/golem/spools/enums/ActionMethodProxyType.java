2ff12e2bb6b31493ce0f606b50b2ddd4a60adf73
maslu02 <maslu02@ca.com>
2015-01-26 20:10:06 +0100

    General Licence header for project created and applayed on all classes in project.

--- src/cz/a_d/automation/golem/spools/enums/ActionMethodProxyType.java
+++ src/cz/a_d/automation/golem/spools/enums/ActionMethodProxyType.java
@@ -1,4 +1,16 @@
-/*
+/* 
+ *  DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *  
+ *  Copyright 2015 czcaspercz. All rights reserved.
+ *  
+ *  The contents of this file are subject to the terms of either the the Common Development and Distribution License 1.0 ("CDDL 1.0")
+ *  You may not use this file except in compliance with the License. You can obtain a copy of the License at 
+ *  
+ *  http://opensource.org/licenses/CDDL-1.0
+ *  
+ *  See the License for the specific language governing permissions and limitations under the License.
+ *  When distributing the software, include this License Header
+ *  
  */
 package cz.a_d.automation.golem.spools.enums;
 
